c47951212ea9f8edf82d4cc71fc8106ebff7c39f
Tom <tom@tomrijnbeek.nl>
2015-12-13 21:10:11 +0100

    Remove shadow when tree is cut.

--- Assets/Scripts/Game/Plants/TreePlant.cs
+++ Assets/Scripts/Game/Plants/TreePlant.cs
@@ -7,4 +7,13 @@ public class TreePlant : Plant {
         GameManager.Instance.currency += score;
         // Don't destroy
     }
+
+    protected override void DestroyPlant()
+    {
+        var tile = GetComponentInParent<Tile>();
+        foreach (var t in tile.AdjacentTiles())
+            t.RemoveEffect(Tile.TileEffects.Shadow);
+
+        base.DestroyPlant();
+    }
 }
